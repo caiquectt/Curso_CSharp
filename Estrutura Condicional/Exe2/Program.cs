c644203f5b10543044e657b6156fe6a7ca2da014
caiquectt <caiquectt@gmail.com>
2021-03-03 09:32:14 -0300

    Estrutura Condicional

---
+++ Estrutura Condicional/Exe2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0){
+                System.Console.WriteLine("PAR");
+            }
+            else{
+                System.Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
