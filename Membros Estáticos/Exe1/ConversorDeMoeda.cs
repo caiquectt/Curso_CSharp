000f7318cbc8da3ef2a35ad0be4c5c9dbe5625eb
caiquectt <caiquectt@gmail.com>
2021-06-23 10:47:35 -0300

    Membros Estáticos

---
+++ Membros Estáticos/Exe1/ConversorDeMoeda.cs
@@ -0,0 +1,9 @@
+namespace Exe1
+{
+    public class ConversorDeMoeda
+    {
+        public static double Conversao(double quant, double valor){
+            return (valor * quant) + (valor * quant) * 0.06;
+        }
+    }
+}
---
+++ Membros Estáticos/Exe1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a cotação do dolar?");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("Quantos dólares você vai comprar?");
+            double quant = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("Valor a ser pago em reais: " + ConversorDeMoeda.Conversao(quant, dolar).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
