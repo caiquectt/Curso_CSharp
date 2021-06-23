e7f85cef2e4a1d250839647a056023a798269472
caiquectt <caiquectt@gmail.com>
2021-06-23 10:47:16 -0300

    .

--- Membros Estáticos/Exe1/ConversorDeMoeda.cs
+++
@@ -1,9 +0,0 @@
-namespace Exe1
-{
-    public class ConversorDeMoeda
-    {
-        public static double Conversao(double quant, double valor){
-            return (valor * quant) + (valor * quant) * 0.06;
-        }
-    }
-}
