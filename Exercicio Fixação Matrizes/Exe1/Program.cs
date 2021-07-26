a627047786cc6bfc338b414ed3b8f88eafff7381
caiquectt <caiquectt@gmail.com>
2021-07-26 09:44:42 -0300

    Exercicio Fixação (matrizes)

---
+++ Exercicio Fixação Matrizes/Exe1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Digite um número para ordem da matriz: ");
+            int ordem = int.Parse(Console.ReadLine());
+            
+            int[,] mat = new int [ordem, ordem];
+            int negativo = 0;
+            for (int i=0; i<ordem; i++){
+                System.Console.WriteLine("Digite o(s) " + ordem + " número(s) da " + (i+1) + "a linha");
+                string[] vet = Console.ReadLine().Split(' ');
+                for (int j=0; j<ordem; j++){
+                    mat[i,j] = int.Parse(vet[j]);
+                    if (int.Parse(vet[j]) < 0){
+                        negativo++;
+                    }
+                }
+            }
+
+            System.Console.WriteLine("Diagonal principal:");
+            for (int i=0; i<ordem; i++){
+                System.Console.Write(mat[i,i] + " ");
+            }
+            System.Console.WriteLine();
+            System.Console.Write("Quantidade de números negativos: " + negativo);
+        }
+    }
+}
---
+++ Exercicio Fixação Matrizes/Exe2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o número de linhas e colunas: ");
+            string[] tamanho = Console.ReadLine().Split(' ');
+            int m = int.Parse(tamanho[0]);
+            int n = int.Parse(tamanho[1]);
+
+            int[,] mat = new int [m,n];
+
+            for (int i=0; i<m; i++){
+                System.Console.WriteLine("Digite o(s) " + n + " número(s) da " + (i+1) + "a linha:");
+                string[] vet = Console.ReadLine().Split(' ');
+                for (int j=0; j<n; j++){
+                    mat[i,j] = int.Parse(vet[j]);
+                }
+            }
+
+            System.Console.WriteLine("Digite um número pertencente a matriz: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i=0; i<m; i++){
+                for (int j=0; j<n; j++){
+                    if (mat[i,j] == num){
+                        System.Console.WriteLine("Posição " + i + "," + j + " :");
+                        if (j > 0){
+                            System.Console.WriteLine("Esquerda: " + mat[i,j-1]);
+                        }
+                        if (j < n-1){
+                            System.Console.WriteLine("Direita: " + mat[i,j+1]);
+                        }
+                        if (i > 0){
+                            System.Console.WriteLine("Cima: " + mat[i-1,j]);
+                        }
+                        if (i < m-1){
+                            System.Console.WriteLine("Baixo: " + mat[i+1,j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
